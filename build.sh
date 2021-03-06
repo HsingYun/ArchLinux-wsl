ca227e07cd4e536d631ed6a05cd7a6977d5d5758
HsingYun <iakext@gmail.com>
2021-03-06 16:32:58 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,9 +5,13 @@ git fetch
 cd ../src/archlinux-linux
 git add . && git reset --hard && git pull
 cd ../../..
+mv linux linux_src
 echo "begin update and export the linux pkg"
 asp update linux
 asp export linux
+cp linux/* linux_src
+rm -rf linux
+mv linux_src linux
 echo "begin modify the PKGBUILD and the config"
 sed -i 's/pkgbase=linux$/pkgbase=linux-wsl/' linux/PKGBUILD
 sed -i 's/make all$/make all -j24/' linux/PKGBUILD
